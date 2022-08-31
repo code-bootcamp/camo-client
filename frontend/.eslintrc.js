bc817da4e6e22b386b6b8ae679245ea57920f7e4
jaein0304 <ddc0406@naver.com>
2022-08-31 20:16:47 +0900

    Add: frontend folder structure

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ["plugin:react/recommended", "standard", "prettier"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ["react"],
   rules: {
-  }
-}
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/display-name": "off",
+  },
+  settings: {
+    react: {
+      version: "17.0.2",
+    },
+  },
+};
