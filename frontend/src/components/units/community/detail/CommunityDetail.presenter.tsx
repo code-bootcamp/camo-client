4d18c33a4585e4b7507710576b00d57210e96f53
sky8130813 <sky8130813@gmail.com>
2022-09-07 11:45:07 +0900

    Update: community write, list, detail css

---
+++ frontend/src/components/units/community/detail/CommunityDetail.presenter.tsx
@@ -0,0 +1,39 @@
+import * as S from "./CommunityDetail.styles";
+
+export default function CommunityDetailUI() {
+  return (
+    <S.Wrapper>
+      <S.TitleWrapper>
+        <S.Title>오늘 친구들과 같이 달콤 카페를 다녀와 봤습니다.</S.Title>
+        <S.UserWrapper>
+          <S.UserImg></S.UserImg>
+          <S.UserInfoWrapper>
+            <S.UserName>홍길동</S.UserName>
+            <S.PostWrapper>
+              <S.PostDetail>2022.8.6 16:30</S.PostDetail>
+              <S.PostDetail>조회: 80</S.PostDetail>
+              <S.PostDetail>댓글: 30</S.PostDetail>
+            </S.PostWrapper>
+          </S.UserInfoWrapper>
+        </S.UserWrapper>
+      </S.TitleWrapper>
+      <S.ContentsWrapper>
+        <S.MainImg></S.MainImg>
+        <S.ImgWrapper>
+          <S.SubImg></S.SubImg>
+          <S.SubImg></S.SubImg>
+          <S.SubImg></S.SubImg>
+          <S.SubImg></S.SubImg>
+        </S.ImgWrapper>
+        <S.Contents>내용</S.Contents>
+        <S.Tags>#태그</S.Tags>
+        <S.ContentsIcons>
+          <S.CommentIcon />
+          30
+          <S.LikeIcon />
+          70
+        </S.ContentsIcons>
+      </S.ContentsWrapper>
+    </S.Wrapper>
+  );
+}
---
+++ frontend/src/components/units/community/detail/CommunityDetail.styles.ts
@@ -0,0 +1,57 @@
+import { LikeOutlined, MessageOutlined } from "@ant-design/icons";
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 100vw;
+  background-color: #fff;
+  padding: 10%;
+`;
+
+export const TitleWrapper = styled.div`
+  width: 100%;
+  padding: 1rem 0;
+`;
+
+export const Title = styled.h1`
+  font-weight: 700;
+`;
+
+export const UserWrapper = styled.div`
+  width: 100%;
+`;
+
+export const UserImg = styled.div``;
+
+export const UserInfoWrapper = styled.div`
+  width: 100%;
+`;
+
+export const UserName = styled.div``;
+
+export const PostWrapper = styled.div`
+  width: 100%;
+`;
+
+export const PostDetail = styled.div``;
+
+export const ContentsWrapper = styled.div`
+  width: 100%;
+`;
+
+export const MainImg = styled.div``;
+
+export const ImgWrapper = styled.div`
+  width: 100%;
+`;
+
+export const SubImg = styled.div``;
+
+export const Contents = styled.div``;
+
+export const Tags = styled.div``;
+
+export const ContentsIcons = styled.div``;
+
+export const CommentIcon = styled(MessageOutlined)``;
+
+export const LikeIcon = styled(LikeOutlined)``;
