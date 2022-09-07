dd77acb3b6a5883f1b2a5efabae9c958a25f1a20
jaein0304 <ddc0406@naver.com>
2022-09-07 12:33:36 +0900

    Fix: header presenter conflict

--- frontend/src/components/commons/layout/header/LayoutHeader.presenter.tsx
+++ frontend/src/components/commons/layout/header/LayoutHeader.presenter.tsx
@@ -1,3 +1,4 @@
+import { MenuOutlined } from "@ant-design/icons";
 import Link from "next/link";
 import {
   HeaderWrapper,
@@ -5,7 +6,9 @@ import {
   LoginUser,
   LoginWrapper,
   Logo,
+  LogoMenuWrapper,
   MenuFonts,
+  MenuTab,
   MenuWrapper,
 } from "./LayoutHeader.style";
 import { ILayoutHeaderUIProps } from "./LayoutHeader.types";
@@ -14,41 +17,55 @@ export default function LayoutHeaderUI(props: ILayoutHeaderUIProps) {
   return (
     <>
       <HeaderWrapper>
-        <h1>
-          <Link href="/">
-            <a>
-              <Logo src="/Header_CAMO.png" alt="logo" />
-            </a>
-          </Link>
-        </h1>
-
-        <MenuWrapper>
-          <Link href="/cafe">
-            <a>
-              <MenuFonts>Reservation</MenuFonts>
-            </a>
-          </Link>
-
-          <Link href="/community">
-            <a>
-              <MenuFonts>Community</MenuFonts>
-            </a>
-          </Link>
-          {props.accessToken ? (
-            <Link href="/myPage/myLike">
+        <LogoMenuWrapper>
+          <h1>
+            <Link href="/">
               <a>
-                <MenuFonts>My page</MenuFonts>
+                <Logo src="/Header_CAMO.png" alt="logo" />
               </a>
             </Link>
-          ) : (
-            <a></a>
-          )}
-        </MenuWrapper>
+          </h1>
+
+          <MenuWrapper>
+            <Link href="/cafe">
+              <a>
+                <MenuFonts>HOME</MenuFonts>
+              </a>
+            </Link>
+            <Link href="/cafe">
+              <a>
+                <MenuFonts>ABOUT</MenuFonts>
+              </a>
+            </Link>
+
+            <Link href="/cafe">
+              <a>
+                <MenuFonts>RESERVATION</MenuFonts>
+              </a>
+            </Link>
+
+            <Link href="/community">
+              <a>
+                <MenuFonts>COMMUNITY</MenuFonts>
+              </a>
+            </Link>
+            {props.accessToken ? (
+              <Link href="/myPage/myLike">
+                <a>
+                  <MenuFonts>My page</MenuFonts>
+                </a>
+              </Link>
+            ) : (
+              <a></a>
+            )}
+          </MenuWrapper>
+        </LogoMenuWrapper>
 
         {props.accessToken ? (
           <LoginWrapper>
-            <LoginUser>{props.data?.fetchLoginUser.name}님</LoginUser>
+            <LoginUser>{props.data?.fetchUser.name}님</LoginUser>
             <LoginButton onClick={props.onClickLogout}>로그아웃</LoginButton>
+            <MenuTab />
           </LoginWrapper>
         ) : (
           <LoginWrapper>
@@ -63,6 +80,7 @@ export default function LayoutHeaderUI(props: ILayoutHeaderUIProps) {
                 <LoginButton>회원가입</LoginButton>
               </a>
             </Link>
+            <MenuTab />
           </LoginWrapper>
         )}
       </HeaderWrapper>
