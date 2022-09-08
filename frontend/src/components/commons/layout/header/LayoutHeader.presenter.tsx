ec10997258428ee781d08c1c0b43d2da2dc5d44d
yeeunit <107978408+yeeunit@users.noreply.github.com>
2022-09-08 12:23:48 +0900

    Update LayoutHeader.presenter.tsx

--- frontend/src/components/commons/layout/header/LayoutHeader.presenter.tsx
+++ frontend/src/components/commons/layout/header/LayoutHeader.presenter.tsx
@@ -1,3 +1,4 @@
+import { MenuOutlined } from "@ant-design/icons";
 import Link from "next/link";
 import {
   HeaderWrapper,
@@ -9,84 +10,80 @@ import {
   MenuFonts,
   MenuTab,
   MenuWrapper,
-  Wrapper,
 } from "./LayoutHeader.style";
 import { ILayoutHeaderUIProps } from "./LayoutHeader.types";
 
 export default function LayoutHeaderUI(props: ILayoutHeaderUIProps) {
   return (
     <>
-      <Wrapper>
-        <HeaderWrapper>
-          <LogoMenuWrapper>
-            <h1>
-              <Link href="/">
-                <a>
-                  <Logo src="/logo_test.png" alt="logo" />
-                </a>
-              </Link>
-            </h1>
+      <HeaderWrapper>
+        <LogoMenuWrapper>
+          <h1>
+            <Link href="/">
+              <a>
+                <Logo src="/Header_CAMO.png" alt="logo" />
+              </a>
+            </Link>
+          </h1>
 
-            <MenuWrapper>
-              <Link href="/cafe">
-                <a>
-                  <MenuFonts>HOME</MenuFonts>
-                </a>
-              </Link>
-              <Link href="/cafe">
-                <a>
-                  <MenuFonts>ABOUT</MenuFonts>
-                </a>
-              </Link>
+          <MenuWrapper>
+            <Link href="/cafe">
+              <a>
+                <MenuFonts>HOME</MenuFonts>
+              </a>
+            </Link>
+            <Link href="/cafe">
+              <a>
+                <MenuFonts>ABOUT</MenuFonts>
+              </a>
+            </Link>
 
-              <Link href="/cafe">
+            <Link href="/cafe">
+              <a>
+                <MenuFonts>RESERVATION</MenuFonts>
+              </a>
+            </Link>
+
+            <Link href="/community">
+              <a>
+                <MenuFonts>COMMUNITY</MenuFonts>
+              </a>
+            </Link>
+            {props.accessToken ? (
+              <Link href="/myPage/myLike">
                 <a>
-                  <MenuFonts>RESERVATION</MenuFonts>
+                  <MenuFonts>My page</MenuFonts>
                 </a>
               </Link>
+            ) : (
+              <a></a>
+            )}
+          </MenuWrapper>
+        </LogoMenuWrapper>
 
-              <Link href="/community">
-                <a>
-                  <MenuFonts>COMMUNITY</MenuFonts>
-                </a>
-              </Link>
-              {props.accessToken ? (
-                <Link href="/myPage/myLike">
-                  <a>
-                    <MenuFonts>My page</MenuFonts>
-                  </a>
-                </Link>
-              ) : (
-                <a></a>
-              )}
-            </MenuWrapper>
-          </LogoMenuWrapper>
+        {props.accessToken ? (
+          <LoginWrapper>
+            <LoginUser>{props.data?.fetchLoginUser.name}님</LoginUser>
+            <LoginButton onClick={props.onClickLogout}>로그아웃</LoginButton>
+            <MenuTab />
+          </LoginWrapper>
+        ) : (
+          <LoginWrapper>
+            <Link href="/login">
+              <a>
+                <LoginButton>로그인</LoginButton>
+              </a>
+            </Link>
 
-          {props.accessToken ? (
-            <LoginWrapper>
-              <LoginUser>{props.data?.fetchLoginUser.name}님</LoginUser>
-
-              <LoginButton onClick={props.onClickLogout}>로그아웃</LoginButton>
-              <MenuTab />
-            </LoginWrapper>
-          ) : (
-            <LoginWrapper>
-              <Link href="/login">
-                <a>
-                  <LoginButton>로그인</LoginButton>
-                </a>
-              </Link>
-
-              <Link href="/login/signUp">
-                <a>
-                  <LoginButton>회원가입</LoginButton>
-                </a>
-              </Link>
-              <MenuTab />
-            </LoginWrapper>
-          )}
-        </HeaderWrapper>
-      </Wrapper>
+            <Link href="/login/signUp">
+              <a>
+                <LoginButton>회원가입</LoginButton>
+              </a>
+            </Link>
+            <MenuTab />
+          </LoginWrapper>
+        )}
+      </HeaderWrapper>
     </>
   );
 }
