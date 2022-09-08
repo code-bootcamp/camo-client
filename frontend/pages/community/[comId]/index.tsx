254e73c905d94cadee0e77433a31e163574457c8
yeeunit <107978408+yeeunit@users.noreply.github.com>
2022-09-08 12:23:29 +0900

    Update index.tsx

--- frontend/pages/community/[comId]/index.tsx
+++ frontend/pages/community/[comId]/index.tsx
@@ -1,13 +1,1 @@
-// 커뮤니티 게시글 디테일 페이지
 
-import CommunityDetail from "../../../src/components/units/community/detail/CommunityDetail.container";
-
-export default function CafeDetailPage() {
-  return (
-    <>
-      <CommunityDetail />
-    </>
-  );
-}
-
-// export default withAuth(CafeDetailPage)
