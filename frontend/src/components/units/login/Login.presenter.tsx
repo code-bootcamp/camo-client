ca9e5d05bfccb9e05c9afadd980383c202de2fe7
jaein0304 <ddc0406@naver.com>
2022-09-10 04:35:56 +0900

    Update: social login

--- frontend/src/components/units/login/Login.presenter.tsx
+++ frontend/src/components/units/login/Login.presenter.tsx
@@ -1,20 +1,20 @@
 import * as S from "./Login.styles";
 import * as B from "../../commons/wrapper/LoginWrapper.styles";
 import { ILoginUIProps } from "./Login.types";
-import { Error } from "./signUp/SignUp.styles";
+import { Error, Form } from "./signUp/SignUp.styles";
 import Link from "next/link";
 
 export default function LoginUI(props: ILoginUIProps) {
   return (
     <>
       <B.Wrapper>
-        <B.SideWrapper>
+        {/* <B.SideWrapper>
           <B.ColumnWrapper>
             <B.SubLogo>당신이 원하는 카페들을 모은</B.SubLogo>
             <B.SideLogo src="/CAMO.png" />
           </B.ColumnWrapper>
-        </B.SideWrapper>
-        <form onSubmit={props.handleSubmit(props.onClickLogin)}>
+        </B.SideWrapper> */}
+        <Form onSubmit={props.handleSubmit(props.onClickLogin)}>
           <S.Wrapper>
             <B.Title>로그인</B.Title>
             <S.SubTitle>당신이 아는 카페들을 모은 카모</S.SubTitle>
@@ -24,14 +24,20 @@ export default function LoginUI(props: ILoginUIProps) {
               <Error>{props.formState.errors.email?.message}</Error>
               <B.Input type="password" placeholder="비밀번호" {...props.register("password")} />
               <Error>{props.formState.errors.password?.message}</Error>
-              <S.SocialLoginWrapper>
-                <B.SocialLogin src="/Kakao.png" />
-                <B.SocialLogin src="/Naver.png" />
-                <B.SocialLogin src="/Facebook.png" />
-              </S.SocialLoginWrapper>
               <B.ButtonWrapper>
                 <B.SubmitButton>로그인</B.SubmitButton>
               </B.ButtonWrapper>
+              <S.SocialLoginWrapper>
+                <B.SocialA href="https://cafemoment-backend.site/login/google">
+                  <S.GoogleLogin src="/Google.png" />
+                </B.SocialA>
+                <B.SocialA href="https://cafemoment-backend.site/login/kakao">
+                  <S.SocialLogin src="/Kakao.png" />
+                </B.SocialA>
+                <B.SocialA href="https://cafemoment-backend.site/login/naver">
+                  <S.SocialLogin src="/Naver.png" />
+                </B.SocialA>
+              </S.SocialLoginWrapper>
               <S.FooterWrapper>
                 <S.RowWrapper>
                   <S.FooterText>아직 계정이 없으신가요?</S.FooterText>
@@ -52,7 +58,7 @@ export default function LoginUI(props: ILoginUIProps) {
               </S.FooterWrapper>
             </S.MainWrapper>
           </S.Wrapper>
-        </form>
+        </Form>
       </B.Wrapper>
     </>
   );
