6f0b87b85ca5fcc70d22193f471256b08dae9466
Jaein <44810842+jaein0304@users.noreply.github.com>
2022-09-19 17:37:42 +0900

    Update MyWrite.presenter.tsx

--- frontend/src/components/units/myPage/myWrite/MyWrite.presenter.tsx
+++ frontend/src/components/units/myPage/myWrite/MyWrite.presenter.tsx
@@ -1,15 +1,8 @@
-import LayoutSidebar from "../../../commons/layout/sidebar/LayoutSidebar.container";
-import * as B from "../MyPage.styles";
-import * as S from "./MyWrite.styles";
 import { v4 as uuidv4 } from "uuid";
 import { IBoard } from "../../../../commons/types/generated/types";
 import { IMyWriteUIProps } from "./MyWrite.types";
-import PaginationContainer from "../myReservation/pagination/Pagination.container";
-import { PaginationWrapper } from "../myReservation/MyReservation.styles";
 
 export default function MyWriteUI(props: IMyWriteUIProps) {
-  console.log(props.WriteData?.fetchUserMyBoard1);
-
   return (
     <>
       <B.Wrapper>
@@ -17,28 +10,14 @@ export default function MyWriteUI(props: IMyWriteUIProps) {
         <B.Body>
           <B.StayMenu>내가 쓴 글</B.StayMenu>
           <B.Line />
-          {props.WriteData?.fetchUserMyBoard1?.map((el: IBoard) => (
+          {props.WriteData?.fetchUserbyId?.board?.map((el: IBoard) => (
             <S.MainWrapper key={uuidv4()}>
               <S.RowWrapper>
                 <S.ContentBackGround>
-                  <S.ContentWrapper>
-                    <S.Title>{el.title}</S.Title>
-                    <S.RowWrapper>
-                      <S.RightContent>{el.contents}</S.RightContent>
-                    </S.RowWrapper>
-                  </S.ContentWrapper>
-                </S.ContentBackGround>
+	@@ -27,6 +31,12 @@ export default function MyWriteUI(props: IMyWriteUIProps) {
               </S.RowWrapper>
             </S.MainWrapper>
           ))}
-          <PaginationWrapper>
-            <PaginationContainer
-              refetch={props.refetch}
-              count={props.count?.fetchCafeReservationNumber}
-            />
-          </PaginationWrapper>
         </B.Body>
       </B.Wrapper>
     </>
-  );
-}
