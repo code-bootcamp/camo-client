d3cfd694f4542dfe3bb292da4752ec86c2a8109b
Jaein <44810842+jaein0304@users.noreply.github.com>
2022-09-19 17:37:24 +0900

    Update MyWrite.container.tsx

--- frontend/src/components/units/myPage/myWrite/MyWrite.container.tsx
+++ frontend/src/components/units/myPage/myWrite/MyWrite.container.tsx
@@ -1,28 +1,26 @@
 import { useQuery } from "@apollo/client";
-import { IQuery, IQueryFetchUserMyBoard1Args } from "../../../../commons/types/generated/types";
+import { IQuery, IQueryFetchUserbyIdArgs } from "../../../../commons/types/generated/types";
 import useAuth from "../../../commons/hooks";
 import { FETCH_LOGINED_USER } from "../MyPage.queries";
 import MyWriteUI from "./MyWrite.presenter";
-import { FETCH_USER_MY_BOARD, FETCH_USER_MY_BOARD_NUMBER } from "./MyWrite.queries";
+import { FETCH_USER_BY_ID } from "./MyWrite.queries";
 
 /**
  * 재인 수정 중 ..
- * Date: 0919 02:37
- * API 필요
+ * Date: 0916 22:19
  */
 
 export default function MyWrite() {
   useAuth();
 
   const { data: UserData } = useQuery<Pick<IQuery, "fetchLoginedUser">>(FETCH_LOGINED_USER);
-  const { data: count } = useQuery(FETCH_USER_MY_BOARD_NUMBER);
-  const { data: WriteData, refetch } = useQuery<
-    Pick<IQuery, "fetchUserMyBoard1">,
-    IQueryFetchUserMyBoard1Args
-  >(FETCH_USER_MY_BOARD, {
-    variables: {
-      userId: String(UserData?.fetchLoginedUser.id),
-    },
-  });
-  return <MyWriteUI UserData={UserData} WriteData={WriteData} count={count} refetch={refetch} />;
+  const { data: WriteData } = useQuery<Pick<IQuery, "fetchUserbyId">, IQueryFetchUserbyIdArgs>(
+    FETCH_USER_BY_ID,
+    {
+      variables: {
+        userId: String(UserData?.fetchLoginedUser.id),
+      },
+    }
+  );
+  return <MyWriteUI UserData={UserData} WriteData={WriteData} />;
 }
