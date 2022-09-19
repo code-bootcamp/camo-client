2873b9272af133028a003b0d48c1734f7a503262
Jaein <44810842+jaein0304@users.noreply.github.com>
2022-09-19 17:38:34 +0900

    Update MyWrite.types.ts

--- frontend/src/components/units/myPage/myWrite/MyWrite.types.ts
+++ frontend/src/components/units/myPage/myWrite/MyWrite.types.ts
@@ -1,8 +1,5 @@
-import { IQuery } from "../../../../commons/types/generated/types";
 
 export interface IMyWriteUIProps {
   UserData: Pick<IQuery, "fetchLoginedUser"> | undefined;
-  WriteData: any;
-  count: any;
-  refetch: any;
+  WriteData: Pick<IQuery, "fetchUserbyId"> | undefined;
 }
